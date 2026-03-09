bbd9debafc95f7e700554fe904f8f6c83d1bd083
badawishii74-dev <badawishii74@gmail.com>
2026-03-09 19:46:30 +0200

    fix: update Vite proxy target to use production URL for API requests

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://saas-task-management-api.onrender.com',
-        target: 'http://localhost:3000',
+        target: 'https://saas-task-management-api.onrender.com',
+        // target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
